2355cbb01ce5c1a26ebfbe3ac9fb5f0f01af6dde
Ozodbek2129 <ozodbekshamsiddinov1504@gmail.com>
2024-08-27 15:29:01 +0500

    websocket

--- main.go
+++ main.go
@@ -13,42 +13,43 @@ var Upgrader = websocket.Upgrader{
 	WriteBufferSize: 1024,
 }
 
-type Message struct{
-	Name string `json:"name"`
+type Message struct {
+	Name    string `json:"name"`
 	Message string `json:"message"`
 }
 
 func main() {
 	mux := http.NewServeMux()
 
-	mux.HandleFunc("/",Handler_conection)
+	mux.HandleFunc("/", Handler_conection)
 	fmt.Println("Server Started >>> localhost:3000")
-	err := http.ListenAndServe("localhost:3000",mux)
-	if err!=nil{
-		log.Fatal("error: ",err)
+	err := http.ListenAndServe("localhost:3000", mux)
+	if err != nil {
+		log.Fatal("error: ", err)
 	}
 }
 
 func Handler_conection(w http.ResponseWriter, r *http.Request) {
 	websocketCon, err := Upgrader.Upgrade(w, r, nil)
-	if err!=nil{
-		log.Fatal("error: ",err)
+	if err != nil {
+		log.Fatal("error: ", err)
 	}
 
+	address := websocketCon.NetConn()
 	defer websocketCon.Close()
 
 	for {
 		var msg Message
 		err := websocketCon.ReadJSON(&msg)
-		if err!=nil{
-			log.Fatal("error: ",err)
+		if err != nil {
+			log.Fatal("error: ", err)
 		}
 
-		log.Println("Message from user: ",msg.Name, "Message: ",msg.Message)
+		log.Println("Message from user: ", msg.Name, "Address: ", address.RemoteAddr(), "Message: ", msg.Message)
 
 		err = websocketCon.WriteJSON(&msg)
-		if err!=nil{
-			log.Fatal("error: ",err)
+		if err != nil {
+			log.Fatal("error: ", err)
 		}
 	}
 }
